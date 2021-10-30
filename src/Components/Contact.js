93cabe0a05f4454fbaf592d09928d9281a769b48
Zak Wilson <zakwilson111098@gmail.com>
2021-10-30 20:11:17 +0100

    Serv page, cont page hom page

---
+++ src/Components/Contact.js
@@ -0,0 +1,25 @@
+function Contact() {
+    return (<div className="ContactInfo"> 
+        <h2>Here is some contact information for Jays Boosting Service:</h2><br></br>
+        <ul>
+            <li>
+                If you are need of assistance with an order request please email: <br>
+                </br> <p>TestEmail123@gmail.com</p>
+            </li>
+            <br></br>
+            <li>
+                If you are requesting a refund or cancelation of an order please email: <br>
+                </br> <p>RefundsJBS@gmail.com</p>
+                
+            </li>
+            <br></br>
+            <li>
+                If you have any further enquiries about the services that are provided please email: <br>
+                </br> <p>InfoJBS@gmail.com</p>
+            </li>
+        </ul>
+    </div>)
+    
+    }
+    
+    export default Contact;
